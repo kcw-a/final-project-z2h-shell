8bcd430782665074371726178af40a1227d67495
Anson <48442757+kcw-a@users.noreply.github.com>
2025-06-01 22:50:44 +0800

    add employee

--- src/main.c
+++ src/main.c
@@ -15,19 +15,24 @@ void print_usage(char *argv[]) {
 
 int main(int argc, char *argv[]) {
   char *filepath = NULL;
+  char *addstring = NULL;
   bool newfile = false;
   int c = 0;
 
   int dbfd = -1;
-  struct dbheader_t *header = NULL;
+  struct dbheader_t *dbhdr = NULL;
+  struct employee_t *employees = NULL;
 
-  while ((c = getopt(argc, argv, "nf:")) != -1) {
+  while ((c = getopt(argc, argv, "nf:a:")) != -1) {
     switch (c) {
     case 'n':
       newfile = true;
       break;
     case 'f':
       filepath = optarg;
+      break;
+    case 'a':
+      addstring = optarg;
       break;
     case '?':
       printf("Unknown option: %c\n", optopt);
@@ -51,7 +56,7 @@ int main(int argc, char *argv[]) {
       return -1;
     }
 
-    if (create_db_header(dbfd, &header) == STATUS_ERROR) {
+    if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
       printf("Unable to create database header\n");
       return -1;
     }
@@ -62,15 +67,24 @@ int main(int argc, char *argv[]) {
       return -1;
     }
 
-    if (validate_db_header(dbfd, &header) == STATUS_ERROR) {
+    if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
       printf("Failed to validate database header\n");
       return -1;
     }
   }
 
-  printf("Newfile: %d\n", newfile);
-  printf("Filepath: %s\n", filepath);
+  if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
+    printf("Failed to read employees\n");
+    return 0;
+  }
 
-  struct employee_t *employees = {0};
-  output_file(dbfd, header, employees);
+  if (addstring) {
+    dbhdr->count++;
+    employees = realloc(employees, dbhdr->count * (sizeof(struct employee_t)));
+    add_employee(dbhdr, employees, addstring);
+  }
+
+  output_file(dbfd, dbhdr, employees);
+
+  return 0;
 }
